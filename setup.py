c6f793f47a11c047e0e3f537c42996f25ba688f9
pedrishi <pedro_reis_22@hotmail.com>
2021-01-05 12:02:11 +0000

    changed version scheme to post-release

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     use_scm_version={
         "write_to": "version.py",
         "write_to_template": '__version__ = "{version}"',
+        "version_scheme": "post-release",
     },
     setup_requires=["setuptools_scm"],
 )
